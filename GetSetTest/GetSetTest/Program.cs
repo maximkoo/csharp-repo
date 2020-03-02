6a0dcc148aafa4bf8d07bf9f5ef19ee4445f5112
Копачевский Максим Александрович <mkopachevskiy@rosenergo.com>
2020-03-02 18:01:51 +0300

    Some additions for Evernote

--- GetSetTest/GetSetTest/Program.cs
+++ GetSetTest/GetSetTest/Program.cs
@@ -12,6 +12,7 @@ namespace GetSetTest
 
     public class GetSetTest0
     {
+        //Самый традиционный способ создать методы доступа к private-переменной
         private String x;
         public String getX(){ 
             return x;
@@ -23,7 +24,8 @@ namespace GetSetTest
     }
     public class GetSetTest1 {
         private String x;
-
+        // C# позволяет создать так называемые "свойства", то есть как бы прокси-переменные. Такое свойство позволяет и читать и записывать значение
+        // То есть, если это было бы Руби, то как будто под одним именем Х собраны методы #x и #x=
         public String X {
             get{ 
                 return x;
@@ -37,6 +39,7 @@ namespace GetSetTest
 
     public class GetSetTest2 {
         private String x;
+        // Еще более сокращенная запись, удобно, если в этих методах всё равно ничего умного не написано
         public String X
         {
             get => x;
@@ -45,6 +48,38 @@ namespace GetSetTest
     }
     
     public class GetSetTest3 {
-        public String X { get; set; }            
+        // сверхкороткий вариант
+        public String X { get; set; }
+        // В этом случае истинное свойство (т.е. переменная, в которой хранится значение) скрыто и генерируется автоматически.
+        // Обращение извне происходит через Х, т.е. вот так
+        // GetSetTest3 gst3 = new GetSetTest3();
+        // gst3.X = "100";
+        // String a = gst3.X;
     }
+
+    // Безотносительно вышенаписанного
+    // Способы инициализации переменных при создании объекта
+    public class BlockSet {
+        public int x;
+        public int y;
+    }
+
+    public class BlockSetCall {
+        public void bsCall1()
+        {
+            BlockSet bs1 = new BlockSet();
+            bs1.x = 100;
+            bs1.y = 200;
+        }
+
+        public void bsCall2()
+        { //Также возможен способ, напоминающий передачу блока в метод new в Руби
+            BlockSet bs2 = new BlockSet
+            {
+                x = 100, // <-- запятая!
+                y = 200
+            };
+        }
+    }
+
 }
