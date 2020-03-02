fa619a4e435013b4fd0a9e405aeab794a4faeb2b
Копачевский Максим Александрович <mkopachevskiy@rosenergo.com>
2020-03-02 17:09:15 +0300

    Office attempts (pitiful)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+
+            for (int i = 0; i < args.Length; i++) {
+                Console.WriteLine(args[i]);
+            }
+
+            foreach (string qqq in args)
+            {
+                Console.WriteLine(qqq);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XamarinApp1/XamarinApp1/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace XamarinApp1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private int n = 0;
+        private TextView txtView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, bundle: savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            txtView = FindViewById<TextView>(Resource.Id.textView1);
+            FindViewById<Button>(Resource.Id.button1).Click += (o, e) => txtView.Text = (++n).ToString();
+            FindViewById<Button>(Resource.Id.button2).Click += (o, e) => txtView.Text = (--n).ToString();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
