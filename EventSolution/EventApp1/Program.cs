4287410b1ebd7e16d336728f6095e070ab58e6f8
Копачевский Максим Александрович <mkopachevskiy@rosenergo.com>
2020-03-13 16:53:07 +0300

    Experiments go on

---
+++ EventSolution/EventApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EventApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C1 c1 = new C1();
+            C2 c2 = new C2();
+            c1.e1 += c2.m1;
+            c1.e1 += c2.m2;
+
+            c1.Click();
+
+            c1.e1 -= c2.m2;
+
+            c1.Click();
+        }
+    }
+
+    public class C1 {
+        public delegate void D1();
+        public event D1 e1;
+        public void Click() {
+            e1();
+        }
+    }
+
+    public class C2 {
+        public void m1() {
+            Console.WriteLine("This is m1");
+        }
+
+        public void m2()
+        {
+            Console.WriteLine("This is m2");
+        }       
+    }
+}
---
+++ EventSolution/EventApp2/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventApp2
+{
+    class CD1
+    {
+        public delegate void D1();
+        public D1 q1;
+        public D1 q2;
+        public D1 q3;
+        public D1 q4;
+        public D1 q5;
+
+        // public delegate void D2() = delegate {Console.WriteLine("D2")};
+        public CD1()
+        {
+            q1 = delegate { };
+            q2 = delegate { Console.WriteLine("q2"); };
+            q3 = delegate {     
+                            Console.WriteLine("Multiline");
+                            Console.WriteLine("Output");
+                          };
+            q4 = delegate { M1(); };
+
+            q5 = () => Console.WriteLine("q5 works too");
+            
+            //
+
+            q5+= delegate { Console.WriteLine("Adding more to q5"); };
+            q5 += q4;
+        }
+
+        public void M1() {
+            Console.WriteLine("m1");
+        }
+
+        public void InvokeQ5()
+        {
+            q5();
+        }
+
+        public void InvokeAllQ5() {
+            int a = q5.GetInvocationList().Length;
+            Console.WriteLine($">>>> Length of q5 is {a}");
+            for (int i = 0; i < a; i++) {
+                q5.GetInvocationList()[i].DynamicInvoke();
+                Console.WriteLine($"Type of this item is {q5.GetInvocationList()[i].GetType().ToString()}"); //Type of this item is EventApp2.CD1+D1 
+            }
+            Console.WriteLine($"Type of q5 is {q5.GetType().ToString()}"); //Type of q5 is EventApp2.CD1+D1 То есть q5 - это делегат типа D1 из класса CD1 в решении EventApp2
+        }
+    }
+}
